6dfa8eab0ea04a7b24e3426c00283197b83a96d7
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-04-20 01:58:06 +0700

    companies

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,13 +1,11 @@
-import { IsEmail, IsEmpty, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateCompanyDto {
-  @IsNotEmpty({
-    message: "name can't be empty",
-  })
+  @IsNotEmpty({ message: "name can't be empty" })
   name: string;
 
   @IsNotEmpty({ message: 'address should not be empty' })
-  addess: string;
+  address: string;
 
   @IsNotEmpty({ message: 'description should not be empty' })
   description: string;
