5dac02e0b09ba33d1f3d106eb06555065e5fe72c
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-04-24 03:02:52 +0700

    logout

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -112,10 +112,10 @@ export class UsersService implements OnModuleInit {
 
   /////////////
   async findAll(currentPage: number, limit: number, qs: string) {
-    const { filter, sort, projection, population } = aqp(qs);
+    const { filter, sort, population } = aqp(qs);
 
-    delete filter.page;
-    delete filter.limit;
+    delete filter.current;
+    delete filter.pageSize;
 
     let offset = (+currentPage - 1) * +limit;
     let defaultLimit = +limit ? +limit : 10;
